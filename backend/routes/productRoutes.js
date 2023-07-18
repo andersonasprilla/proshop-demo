ab99690919c488b770de6cce0650586d9c757390
Anderson Asprilla <anderson.asprilla@gmail.com>
2023-07-18 17:17:00 -0400

    custom error middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,7 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+    throw new Error("Some error");
     res.json(products);
   })
 );
@@ -18,9 +19,10 @@ router.get(
 
     if (product) {
       return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Resource not found");
     }
-
-    res.status(404).json({ message: "Product not found" });
   })
 );
 
