a0ced7499c23b6c13507457798e4c58796f43503
Anderson Asprilla <anderson.asprilla@gmail.com>
2023-08-08 20:03:14 -0400

    Order Page

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -13,7 +13,7 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 
 router.route("/mine").get(protect, getMyOrders);
-router.route("/:id").get(protect, admin, getOrderById);
+router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPay);
 router.route("/:id/delivered").put(protect, admin, updateOrderToDelivered);
 
